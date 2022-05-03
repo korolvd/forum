1b7d7094dde43ecc2f3810b061334f41410073fb
korolvd <ffirin@mail.ru>
2022-05-03 15:01:41 +0500

    1. Spring mock [#2096]

--- src/test/java/ru/forum/control/RegistrationControlTest.java
+++ src/test/java/ru/forum/control/RegistrationControlTest.java
@@ -1,16 +1,24 @@
 package ru.forum.control;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.forum.Main;
+import ru.forum.model.User;
+import ru.forum.service.AuthorityService;
+import ru.forum.service.UserService;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.nullValue;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -21,6 +29,12 @@ public class RegistrationControlTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private AuthorityService authorityService;
 
     @Test
     @WithMockUser
@@ -41,5 +55,18 @@ public class RegistrationControlTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("registration"))
                 .andExpect(model().attribute("errMessage", is("Пользователь с такой почтой уже существует")));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnDefaultMessageSave() throws Exception {
+        this.mockMvc.perform(post("/registration")
+                        .param("username", "Guest")
+                        .param("password", "123456"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<User> argument = ArgumentCaptor.forClass(User.class);
+        verify(userService).save(argument.capture());
+        assertThat(argument.getValue().getUsername(), is("Guest"));
     }
 }
